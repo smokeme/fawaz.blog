f145e0ab5289f1f71931d7c9c3f7c465aa017a8c
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2021-01-09 19:16:11 +0300

    Start as dark mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,57 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
-import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
+  useEffect(() => {
+    toggleTheme("dark")
+  }, [])
 
   if (isRootPath) {
     header = (
       <>
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-            </>
+        <h1 className="main-heading">
+          <Link to="/">{title}</Link>
+        </h1>
+      </>
     )
   } else {
     header = (
       <>
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
+        <Link className="header-link-home" to="/">
+          {title}
+        </Link>
       </>
     )
   }
 
   return (
     <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-                <label
-                style={{float:'right'}}>
+      {({ theme, toggleTheme }) => (
+        <div className="global-wrapper" data-is-root-path={isRootPath}>
+          <label style={{ float: "right" }}>
             <input
               type="checkbox"
-              onChange={e => {toggleTheme(e.target.checked ? 'dark' : 'light');}}
-              checked={theme === 'dark'}
-            />{' '}
+              onChange={e => {
+                toggleTheme(e.target.checked ? "dark" : "light")
+              }}
+              checked={theme === "dark"}
+            />{" "}
             Dark mode
           </label>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>)}
-          </ThemeToggler>
+          <header className="global-header">{header}</header>
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+          </footer>
+        </div>
+      )}
+    </ThemeToggler>
   )
 }
 
